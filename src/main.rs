c7887dc6845921181282ec75a29f6b00f74f7843
igbtw <igoronofre@proton.me>
2026-05-31 22:20:05 -0300

    Basic cli and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::env;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let flags = Flags::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Reading {}:", flags.filename);
+
+    let contents = fs::read_to_string(flags.filename)
+        .expect("Something went wrong while trying to read the file.");
+    println!("The contents is:\n{}", contents);
+}
+
+struct Flags {
+    filename: String,
+}
+
+impl Flags {
+    fn new(args: &[String]) -> Result<Flags, &str> {
+        // returns the filename if Ok and a str if error
+        if args.len() < 2 {
+            // run on cargo is tecnically a flag
+            return Err("Not enough arguments/flags.");
+        }
+
+        let filename = args[1].clone();
+
+        Ok(Flags { filename })
+    }
 }
